8a6200a0247bd7ae4a4b9f6da999c6dbf3c85e4e
Death <ricardo.pajaro.bc2d@gmail.com>
2022-07-27 12:04:02 -0500

    fixed colors and add a toolbar in main screen

--- app/src/main/java/com/nullpointer/blogcompose/ui/screens/main/MainScreen.kt
+++ app/src/main/java/com/nullpointer/blogcompose/ui/screens/main/MainScreen.kt
@@ -1,15 +1,18 @@
 package com.nullpointer.blogcompose.ui.screens.main
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavOptionsBuilder
 import androidx.navigation.compose.currentBackStackEntryAsState
+import com.nullpointer.blogcompose.R
 import com.nullpointer.blogcompose.presentation.AuthViewModel
 import com.nullpointer.blogcompose.ui.interfaces.ActionRootDestinations
 import com.nullpointer.blogcompose.ui.navigation.HomeDestinations
@@ -18,6 +21,8 @@ import com.nullpointer.blogcompose.ui.navigation.RootNavGraph
 import com.nullpointer.blogcompose.ui.screens.NavGraphs
 import com.nullpointer.blogcompose.ui.screens.states.MainScreenState
 import com.nullpointer.blogcompose.ui.screens.states.rememberMainScreenState
+import com.nullpointer.blogcompose.ui.share.SimpleToolbar
+import com.nullpointer.blogcompose.ui.share.ToolbarBack
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.dependency
@@ -32,9 +37,8 @@ fun MainScreen(
     mainScreenState: MainScreenState = rememberMainScreenState()
 ) {
     Scaffold(
-        bottomBar = {
-            MainButtonNavigation(navController = mainScreenState.navController)
-        }
+        topBar = { SimpleToolbar(title = stringResource(id = R.string.app_name))},
+        bottomBar = { MainButtonNavigation(navController = mainScreenState.navController) }
     ){ innerPadding ->
         DestinationsNavHost(
             modifier = Modifier.padding(innerPadding),
@@ -77,9 +81,10 @@ private fun MainButtonNavigation(
                     )
                 },
                 label = { Text(stringResource(id = destination.title)) },
-                selectedContentColor = MaterialTheme.colors.primary,
-                unselectedContentColor = LocalContentColor.current,
-                alwaysShowLabel = false
+                selectedContentColor =Color.Red.copy(0.4f),
+                unselectedContentColor = Color.White,
+                alwaysShowLabel = false,
+                modifier = Modifier.background(MaterialTheme.colors.primary)
             )
         }
     }
--- app/src/main/java/com/nullpointer/blogcompose/ui/share/ToolbarBack.kt
+++ app/src/main/java/com/nullpointer/blogcompose/ui/share/ToolbarBack.kt
@@ -1,27 +1,30 @@
 package com.nullpointer.blogcompose.ui.share
 
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import com.nullpointer.blogcompose.R
 
 @Composable
-fun ToolbarBack(title: String, actionBack: (() -> Unit)? = null) {
-    TopAppBar(title = { Text(title) },
+fun ToolbarBack(title: String, actionBack: () -> Unit) {
+    TopAppBar(
+        backgroundColor = MaterialTheme.colors.primary,
+        title = { Text(title) },
+        contentColor = Color.White,
         navigationIcon = {
-            actionBack?.let { action ->
-                IconButton(onClick = { action() }) {
-                    Icon(painterResource(id = R.drawable.ic_arrow_back),
-                        "")
-                }
+            IconButton(onClick =actionBack) {
+                Icon(painterResource(id = R.drawable.ic_arrow_back),
+                    stringResource(id = R.string.description_arrow_back))
             }
         })
 }
 
 @Composable
 fun SimpleToolbar(title: String) {
-    TopAppBar(title = { Text(title) })
+    TopAppBar(
+        contentColor = Color.White,
+        backgroundColor = MaterialTheme.colors.primary,
+        title = { Text(title) })
 }
